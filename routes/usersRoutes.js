f37e0467882b7799eb519391fdfae8ead40310e6
ZeberMVP <rubeen722@gmail.com>
2023-01-23 09:36:23 +0100

    fix/routes

--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -3,12 +3,12 @@ const usersController = require('../controllers/usersController')
 const usersRouter = express.Router();
 
 usersRouter.get('/', usersController.getUsers);
-usersRouter.get('/:username', usersController.getUser);
 usersRouter.get('/total', usersController.totalUsers);
-usersRouter.get('/:country', usersController.usersFrom);
 usersRouter.get('/vehicles', usersController.numberOfVehicles);
-usersRouter.get('/:food', usersController.favoriteFoodIs);
 usersRouter.get('/vehicles', usersController.hasVehicleWith);
+usersRouter.get('/username/:username', usersController.getUser);
+usersRouter.get('/country/:country', usersController.usersFrom);
+usersRouter.get('/food/:food', usersController.favoriteFoodIs);
 usersRouter.post('/', usersController.createUser);
 usersRouter.put('/:username', usersController.updateUser);
 usersRouter.put('/:username/vehicles', usersController.addVehicles);
